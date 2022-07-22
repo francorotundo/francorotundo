99a64f4f55e86ecb4c7245f74e7687580d9b1563
Francisco Rotundo <francorotundo90@gmail.com>
2022-07-22 00:27:49 -0400

    Adding scroll tracking in the nav

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
+// ------- menu responsive -------
+
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
@@ -8,6 +10,7 @@ function closemenu(){
     sidemenu.style.right = "-150px";
 }
 
+// ------- section skills and education ----------
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -21,3 +24,52 @@ function opentab (tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+// -------- Conf with google sheet----------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzCxzhxYx3O_jMx5ZFQi3mpUAJV_u1rkaUGFt5mEHAQ9bjXNcp5ST3RatEnsowMrgIX5Q/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response =>{
+            msg.innerHTML = "Message sent sucessfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
+// -----------------
+
+addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section")
+    const menuitem = document.querySelectorAll(".menu_item")
+
+    const functionObserver = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const itemActual = Array.from(menuitem).find(item => item.dataset.url === entry.target.id)
+                itemActual.classList.add("active")
+                for (const item of menuitem) {
+                    if (item != itemActual) {
+                        item.classList.remove("active")
+                    }
+                }
+            }
+        })
+    }
+
+    const observer = new IntersectionObserver(functionObserver, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.8
+    })
+
+    sections.forEach(section => observer.observe(section))
+})
+
